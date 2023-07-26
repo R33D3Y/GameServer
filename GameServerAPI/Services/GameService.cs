e7f1743828353bcb1a178690b0e7df11ff434f7b
R33D3Y <reedey2708@gmail.com>
2023-07-26 19:37:18 +0100

    Switched to CMD and started Terraria install.

--- GameServerAPI/Services/GameService.cs
+++ GameServerAPI/Services/GameService.cs
@@ -5,29 +5,53 @@ namespace GameServerAPI.Services
     public class GameService
     {
         private Process? _steamCmdProcess;
+        private const string CMDProcess = "cmd.exe";
+        private const string SteamProcess = "steamcmd +login anonymous";
+        private const string SteamLogin = "steamcmd +login anonymous";
+        private const string SteamForceInstall = "+force_install_dir";
+        private const string SteamAppUpdate = "+app_update";
+        private const string GameServers = @"F:\GameServers";
 
-        public void StartSteamCMD()
+        private const string TerrariaLocation = "Terraria";
+        private const string TerrariaId = "105610";
+
+        public async void StartSteamCMD()
         {
-            string steamCmdPath = @"C:\steamcmd\steamcmd.exe";
-
             // Create a new ProcessStartInfo object and set the necessary properties
-            ProcessStartInfo steamCmdProcessInfo = new ProcessStartInfo(steamCmdPath);
-            steamCmdProcessInfo.UseShellExecute = false; // Set to false to redirect standard output
-            steamCmdProcessInfo.RedirectStandardOutput = true;
+            ProcessStartInfo steamCmdProcessInfo = new ProcessStartInfo(CMDProcess);
+            steamCmdProcessInfo.UseShellExecute = false; // Set to false to redirect standard input, output, and error
             steamCmdProcessInfo.RedirectStandardInput = true;
+            steamCmdProcessInfo.RedirectStandardOutput = true;
+            steamCmdProcessInfo.RedirectStandardError = true;
             steamCmdProcessInfo.CreateNoWindow = true; // Set to true to hide the cmd window
 
             // Start the process
             _steamCmdProcess = new Process();
             _steamCmdProcess.StartInfo = steamCmdProcessInfo;
-            _steamCmdProcess.OutputDataReceived += _steamCmdProcess_OutputDataReceived;
-            _steamCmdProcess.BeginOutputReadLine();
+            _steamCmdProcess.OutputDataReceived += SteamCmdProcess_OutputDataReceived;
+            _steamCmdProcess.ErrorDataReceived += SteamCmdProcess_ErrorDataReceived;
             _steamCmdProcess.Start();
+
+            _steamCmdProcess.BeginOutputReadLine();
+            _steamCmdProcess.BeginErrorReadLine();
+
+            SendCommand($"{SteamProcess} {SteamLogin} {SteamForceInstall} {Path.Combine(GameServers, TerrariaLocation)} {SteamAppUpdate} {TerrariaId}");
         }
 
-        private void _steamCmdProcess_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        private void SteamCmdProcess_OutputDataReceived(object sender, DataReceivedEventArgs e)
         {
-            Debug.WriteLine(e.Data);
+            if (!string.IsNullOrEmpty(e.Data))
+            {
+                Debug.WriteLine(e.Data);
+            }
+        }
+
+        private void SteamCmdProcess_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(e.Data))
+            {
+                Debug.WriteLine($"Error: {e.Data}");
+            }
         }
 
         public void SendCommand(string command)
@@ -43,7 +67,6 @@ namespace GameServerAPI.Services
                 Console.WriteLine("SteamCMD process is not running, or the command is empty.");
             }
         }
-
 
         public void StopSteamCMD()
         {
